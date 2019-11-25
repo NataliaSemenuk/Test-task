c8f053d35d2ba96d1f3d3c80c9531a5dd7a385b0
NataSemenuck-BELITSOFT <nata.semenuck@gmail.com>
2019-11-25 23:09:26 +0300

    Added some corrections

--- index.js
+++ index.js
@@ -11,7 +11,23 @@ const edwardDecrypt = (message) => {
     }
     displayResults(results);
 }
+
+
+// The second variant
+
+// const edwardDecrypt = (message) => {
+//     let results = '';
+//     let i = 0;
+//     while(i < message.length)  {
+//         if(message[i] !== message[i+1] && message[i-1] !== message[i]) {
+//             results += message[i];
+//         }
+//         i++;
+//     }
+//     displayResults(results);
+// }
+
 const displayResults = (decryptedMessage) => {
     const outputField = document.getElementById("decryptedMessage");
-    outputField.innerHTML = decryptedMessage;
+    outputField.innerText = decryptedMessage;
 }
