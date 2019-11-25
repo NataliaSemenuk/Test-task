597b150dd86faed600676f1a309ba86135501b4d
NataSemenuck-BELITSOFT <nata.semenuck@gmail.com>
2019-11-26 01:07:32 +0300

    Changed function

--- index.js
+++ index.js
@@ -1,31 +1,16 @@
 const edwardDecrypt = (message) => {
     let results = '';
-    for (let i=0; i<message.length; i++) {
-        let temp = message[i];
+    let i = 0;
+    while(i < message.length)  {
         if(message[i] !== message[i+1]) {
             results += message[i];
-        }
-        while(temp === message[i+1] && i<message.length) {
             i++;
-        }   
+            continue;
+        } 
+        i+=2;
     }
     displayResults(results);
 }
-
-
-// The second variant
-
-// const edwardDecrypt = (message) => {
-//     let results = '';
-//     let i = 0;
-//     while(i < message.length)  {
-//         if(message[i] !== message[i+1] && message[i-1] !== message[i]) {
-//             results += message[i];
-//         }
-//         i++;
-//     }
-//     displayResults(results);
-// }
 
 const displayResults = (decryptedMessage) => {
     const outputField = document.getElementById("decryptedMessage");
