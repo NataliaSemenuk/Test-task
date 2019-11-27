ddf735d566012377cec02014da4a266455ea9a8c
NataSemenuck-BELITSOFT <nata.semenuck@gmail.com>
2019-11-27 17:02:35 +0300

    Answer without recursion

--- index.js
+++ index.js
@@ -1,25 +1,61 @@
-const edwardDecrypt = (message) => {
-    let results = '';
-    let i = 0;
-    let flag = false;
+// const edwardDecrypt = (message) => {
+//     let results = '';
+//     let i = 0;
+//     let flag = false;
 
-    while(i < message.length)  {
-        if(message[i] !== message[i+1]) {
-            results += message[i];
-            i++;
-            continue;
-        } 
-        i+=2;
-        flag = true;
-    }
-    if (flag == true) {
+//     while(i < message.length)  {
+//         if(message[i] !== message[i+1]) {
+//             results += message[i];
+//             i++;
+//             continue;
+//         } 
+//         i+=2;
+//         flag = true;
+//     }
+//     if (flag == true) {
+//         flag = false;
+//             setTimeout(() => {
+//                 edwardDecrypt(results);
+//             }, 0);
+        
+//     } else {
+//         displayResults(results);
+        
+//     }
+// }
+
+
+//Answer without recursion
+
+const edwardDecrypt = (message) => {
+    let flag = true;
+    let results = '';
+    while (flag == true) {
+        let i = 0;
         flag = false;
-        edwardDecrypt (results);
-    } else {
-        displayResults(results);
+        results = '';
+        while(i < message.length)  {
+            if(message[i] !== message[i+1]) {
+                results += message[i];
+                i++;
+                continue;
+            } 
+            i+=2;
+            flag = true;
+        }
+        message = results;
     }
+    displayResults(results);
 }
+
+
 const displayResults = (decryptedMessage) => {
     const outputField = document.getElementById("decryptedMessage");
     outputField.innerHTML = decryptedMessage;
 }
+// let res = '';
+// for( let i=0; i<10000;i++) {
+//     res += '1234567899876543210'
+    
+//     }
+//     console.log(res)
